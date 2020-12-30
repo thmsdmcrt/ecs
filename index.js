436f061d44c3614c3f9b5082941b65f6058e4279
Thomas Démocrite <thomas.democrite@pm.me>
2020-12-30 15:33:03 +0100

    feat(index.js): stabilizing API
    Using class syntax, using new way to express Component, World (older ecs) and Query.
    
    BREAKING CHANGE: Full API Change, see documentation

--- index.js
+++ index.js
@@ -1,101 +1,238 @@
 /**
- * Entities/Components relation mapping.
- * @type {Object}
+ * [E_COMP_ARRAY_TYPE description]
+ * @type {String}
  */
-const attachments = {};
+const E_COMP_ARRAY_TYPE = 'expects @param {Array<Object|Function>} components.';
 
 /**
- * Entities store.
- * @constant
- * @type {Object}
+ * [E_TARGET_TYPE description]
+ * @type {String}
  */
-const entities = {};
+const E_TARGET_TYPE = 'expects @param {number} target.';
 
 /**
- * Component mask tracking.
- * @type {Number}
+ * [E_TARGET_UNDEFINED description]
+ * @type {String}
  */
-let bits = 0;
+const E_TARGET_UNDEFINED = 'expects @param {number} target to exist in world.';
 
 /**
- * Create a Component.
- * @param  {any} value Component value.
- * If value is a function, it will be considered as a factory, used to provide by entity value or data.
- * @param  {string} name - Component name.
- * @return {Function} - Component.
+ * [validateComponentArgument description]
+ * @param  {[type]} component [description]
+ * @return {[type]}           [description]
  */
-export function component(value, name) {
-	const bit = ++bits << 1;
-	const fn = (...args) => [typeof value === 'function' ? value(...args) : value, name, bit];
-	return fn.valueOf = () => bit, fn;
+function validateComponentType(component) {
+	return component && (typeof component !== 'object' || typeof component !== 'function');
 }
 
 /**
- * Create an Entity.
- * @param  {...Array} components - Entity's component list.
- * @return {number} - Entity.
+ * [warnUnregistredComponent description]
+ * @param  {[type]} component [description]
+ * @return {[type]}           [description]
  */
-export function entity(...components) {
-	const entity = Object.keys(entities).length;
-	const manager = { entity };
-
-	let mask = 0;
-
-	for (const component of components) {
-		const [value, name, bit] = typeof component === 'function' ? component() : component;
-		if (typeof value !== 'undefined') manager[name || bit] = value;
-		attachments[bit] = attachments[bit] ? [...attachments[bit], entity] : [entity];
-		mask |= bit;
+function warnUnregistredComponent(component) {
+	if (!World.components.has(component)) {
+		console.warn(`Expects ${component} to be registred. Skipping.`);
+		return true;
 	}
-
-	return entities[entity] = {mask, manager}, entity;
+	return false;
 }
 
 /**
- * Create a Query.
- * @param  {...number} bits - Components masks. Could be positive or negative. Zero values are ignored.
- * @return {Iterable} - Results
+ *
  */
-export function query(...bits) {
-	let results = [];
-	let includes = [];
-	let include = 0;
-	let exclude = 0;
-	let matches = [];
-	let length = 0;
+export class World {
+	static default = new World();
+	/**
+	 * [components description]
+	 * @type {Map}
+	 */
+	static components = new Map();
+	/**
+	 * [entities description]
+	 * @type {Array}
+	 */
+	entities = [];
+	/**
+	 * [attachments description]
+	 * @type {Object}
+	 */
+	attachments = {};
+	/**
+	 * [managers description]
+	 * @type {Object}
+	 */
+	managers = {};
+	/**
+	 * [push description]
+	 * @param  {Array}  components [description]
+	 * @param  {[type]} target     [description]
+	 * @return {[type]}            [description]
+	 */
+	push(components = [], target = null) {
+		/* Validate method's arguments */
+		if (!Array.isArray(components) ||
+			!components.every(validateComponentType))
+			throw new TypeError(`world#push ${E_COMP_ARRAY_TYPE}`);
 
-	for (const bit of bits) {
-		if (bit < 0) exclude |= -bit;
-		if (bit > 0) {
-			includes = [...includes, bit];
-			include |= bit;
+		if (target && typeof target !== 'number')
+			throw new TypeError(`world#push ${E_TARGET_TYPE}`);
+
+		if (target && !this.entities[target])
+			throw new TypeError(`world#push ${E_TARGET_UNDEFINED}`);
+
+		/* Body */
+		const entity = target ? target : this.entities.push(0) - 1;
+
+		for (const component of components) {
+			const constructor = typeof component === 'function' ? component : component.constructor;
+
+			if (warnUnregistredComponent(constructor)) continue;
+
+			/* Register entity's component mask */
+			const mask = +constructor;
+			this.entities[entity] |= mask;
+
+			/* Attach entity's component */
+			this.attachments[mask] = this.attachments[mask] || [];
+			if (this.attachments[mask].indexOf(entity) < 0)
+				this.attachments[mask].push(entity);
+
+			/* Register entity's component */
+
+			// Skipping for Tag Component.
+			if (Object.keys(constructor).length === 0 && Object.keys(component).length === 0)
+				continue;
+
+			this.managers[entity] = this.managers[entity] || {};
+			this.managers[entity][mask] = component;
+
+			Object.defineProperty(this.managers[entity], constructor.name.toLowerCase(), {
+				get: () => this.managers[entity][mask]
+			});
+		}
+
+		return entity;
+	}
+	/**
+	 * [pull description]
+	 * @param  {[type]} target     [description]
+	 * @param  {Array}  components [description]
+	 * @return {[type]}            [description]
+	 */
+	pull(target, components = []) {
+		/* Validate method's arguments */
+		if (!target || typeof target !== 'number')
+			throw new TypeError(`world#pull ${E_TARGET_TYPE}`);
+
+		if (!Array.isArray(components) ||
+			!components.every((component) => component && typeof component === 'function'));
+			throw new TypeError('world#pull expects @param {Array<Function>} components.');
+
+		if (!this.entities[target])
+			throw new TypeError(`world#pull ${E_TARGET_UNDEFINED}`);$
+
+		/* Body */
+		if (components.length === 0)
+			/* Reset entity's component mask */
+			this.entities[target] = 0;
+
+		if (components.length > 0) {
+			for (const component of components) {
+				if (warnUnregistredComponent(component)) continue;
+				const mask = +component;
+				/* Detach entity's component */
+				const i = this.attachments[mask].indexOf(entity);
+				if (i >= 0) this.attachments[mask].splice(i, 1);
+
+				/* Unregister entity's component mask */
+				this.entities[target] &= ~mask;
+			}
 		}
 	}
-
-	return {
-		// @yields {Array}
-		*[Symbol.iterator]() {
-			for (const bit of includes) {
-				if (matches.length === 0 ||
-					matches.length > attachments[bit].length) {
-					matches = attachments[bit];
-				}
-			}
-
-			if (matches.length !== length) {
-				length = matches.length;
-				results.length = 0;
-
-				for (const entity of matches) {
-					const {mask, manager} = entities[entity];
-					if ((exclude === 0 && (mask & include) === include) ||
-						(exclude > 0 && (mask & include) === include && (mask & exclude) !== exclude))
-						results.push(manager);
-				}
-			}
-
-			yield* results;
-		}
-	};
 }
 
+/**
+ *
+ */
+export class Component {
+	/**
+	 * [valueOf description]
+	 * @return {[type]} [description]
+	 */
+	static valueOf() {
+		if (!World.components.has(this)) {
+			World.components.set(this, 1 + World.components.size << 1);
+		}
+		return World.components.get(this);
+	}
+}
+
+/**
+ *
+ */
+export class Query {
+	attachments = [];
+	include = 0;
+	exclude = 0;
+	match = [];
+	size = 0;
+	results = [];
+	/**
+	 * [constructor description]
+	 * @param  {Array}  masks [description]
+	 * @return {[type]}       [description]
+	 */
+	constructor(masks = []) {
+		if (masks.some(Number.isNaN))
+			throw new TypeError(`query#constructor expects @param {Array<number>} masks. Corresponding component has not been registered.`);
+
+		for (const mask of masks) {
+			if (mask > 0) {
+				this.include |= mask;
+				this.attachments.push(mask);
+			}
+
+			if (mask < 0) {
+				this.exclude |= -mask;
+			}
+		}
+	}
+	/**
+	 * [iter description]
+	 * @param  {[type]} world [description]
+	 * @return {[type]}       [description]
+	 */
+	*iter(world) {
+		if (!(world instanceof World))
+			throw new TypeError('query#iter expects @param {World} world.');
+
+		for (const mask of this.attachments) {
+			if (!this.match ||
+				this.match.length === 0 ||
+				(world.attachments[mask] && this.match.length > world.attachments[mask].length)) {
+				this.match = world.attachments[mask];
+			}
+		}
+
+		if (this.match.length !== this.size) {
+			this.size = this.match.length;
+			this.results.length = 0;
+		}
+
+		if (this.results.length === 0) {
+			for (const entity of this.match) {
+				const mask = world.entities[entity];
+				const include = (mask & this.include) === this.include;
+
+				if ((this.exclude === 0 && include)/* ||
+					(this.exclude > 0 && include && (mask & this.exclude) !== this.exclude)*/)
+					this.results.push(world.managers[entity]);
+			}
+		}
+
+		yield* this.results;
+	}
+}
+
+export default World.default;
